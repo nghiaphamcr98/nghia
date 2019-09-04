120ae12129190ea4a22bfe5f770718f580a7cf84
Nghia Pham <nghia.pham@risotech.vn>
2019-09-04 13:55:06 +0700

    change laptop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Model, NghiaItem } from "./model";
 
 @Component({
   selector: 'nghia-app',
@@ -10,21 +9,5 @@ import { Model, NghiaItem } from "./model";
 
 export class AppComponent
 {
-  model = new Model();
-  
-  getName()
-  {
-    return this.model.user;
-  }
-  getNghiaItems()
-  {
-    return this.model.items.filter(item => !item.done);
-  }
-  addItem(newItem)
-  {
-    if(newItem != "")
-    {
-      this.model.items.push(new NghiaItem(newItem,false));
-    }
-  }
+
 }
